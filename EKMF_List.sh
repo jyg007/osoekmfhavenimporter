277ac70cc4285b2628d608457838cf679925d55d
root <jyg007@gmail.com>
2026-03-25 19:07:23 -0400

    Add List CKV

---
+++ EKMF_List.sh
@@ -0,0 +1,16 @@
+#curl -X POST http://localhost:8080/FrontendCreateRSAKeyPairRequest
+
+EKMF_JSON=$(cat <<EOF
+{
+  "Content": "",
+  "Signature": "",
+  "Metadata": {
+    "type": "EKMFLIST"
+  }
+}
+EOF
+)
+
+curl -X POST http://localhost:8080/FrontendEKMFCmd \
+  -H "Content-Type: application/json" \
+  -d "$EKMF_JSON"
